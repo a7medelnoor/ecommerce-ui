ade4178070ebccac341d57e308f4d3c3e137e640
ahmed <acc.ahmedstudio@gmail.com>
2023-03-25 23:47:15 +0400

    admin add category, get list of categories

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -2,34 +2,82 @@
    <div>Add Category</div>
    <div class="container">
     <div class="row">
-        <div class="text-center">
-            <h3>Add Category</h3>
+        <div class="col-12 text-center">
+            <h3 class="pt-3">Add Category</h3>
         </div>
     </div>
     <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+
         <form>
      <div class="form-group">
         
-       <label>Category Name</label>
-       <input type="text" class="form-control"/>
+       <label>Name</label>
+       <input type="text" class="form-control" v-model="categoryName"/>
      </div>
 
      <div class="form-group">
         
-        <label>Category Desciption</label>
-        <input type="text" class="form-control"/>
+        <label>Desciption</label>
+        <input type="text" class="form-control" v-model="description"/>
       </div>
 
       <div class="form-group">
         
-        <label>Category Image</label>
-        <input type="text" class="form-control"/>
+        <label>Image</label>
+        <input type="text" class="form-control" v-model="imageUrl"/>
       </div>
+      <button class="btn btn-primary" type="button" @click="addCategory">Submit</button>
         </form>
+      </div>
+      <div class="col-3"></div>
+
     </div>
    </div>
 </template>
 <script>
+   const axios = require("axios");
+   const sweetalert = require("sweetalert");
+
+export default {
+  data(){
+    return {
+      categoryName:"",
+      description:"",
+      imageUrl:""
+    };
+  },
+  methods:{
+    addCategory(){
+      console.log(this.categoryName,this.description);
+      const newCategory = {
+           categoryName: this.categoryName,
+           description: this.description,
+           imageUrl: this.imageUrl,
+      };
+      const baseUrl = "http://localhost:8081";
+
+    axios({
+      method: "post",
+      url:`${baseUrl}/category/create`,
+      data: JSON.stringify(newCategory),
+      headers:{
+           'Content-Type':  'application/json',
+                },
+    }).then(() => {
+       sweetalert({
+        text: 'Category Added Successfully',
+        icon: 'success',
+       })
+    }).catch(err => {
+      console.log(err);
+    })
+    },
+  }
+};
 </script>
+
+
 <style>
 </style>
