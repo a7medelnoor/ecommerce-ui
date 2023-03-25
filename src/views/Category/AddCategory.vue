67aa82915b6e64bc4c566810b98bf09a06d485d7
ahmed <acc.ahmedstudio@gmail.com>
2023-03-25 19:49:29 +0400

    initial commit

---
+++ src/views/Category/AddCategory.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>Add Category</div>
+   <div class="container">
+    <div class="row">
+        <div class="text-center">
+            <h3>Add Category</h3>
+        </div>
+    </div>
+    <div class="row">
+        <form>
+     <div class="form-group">
+        
+       <label>Category Name</label>
+       <input type="text" class="form-control"/>
+     </div>
+
+     <div class="form-group">
+        
+        <label>Category Desciption</label>
+        <input type="text" class="form-control"/>
+      </div>
+
+      <div class="form-group">
+        
+        <label>Category Image</label>
+        <input type="text" class="form-control"/>
+      </div>
+        </form>
+    </div>
+   </div>
+</template>
+<script>
+</script>
+<style>
+</style>
